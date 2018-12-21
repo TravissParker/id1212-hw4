6e56e06806a02a4899bb7e6a60d2bc211379ded5
Mikael <mikaelutterback@outlook.com>
2018-12-21 21:49:10 +0100

    Initial commit, untouched.

---
+++ src/test/java/se/kth/id1212/hw4/Hw4ApplicationTests.java
@@ -0,0 +1,17 @@
+package se.kth.id1212.hw4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hw4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
